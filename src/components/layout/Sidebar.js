a09fcbf57d05d64e44f4ca921378ea3ecad2d455
sedx876 <swatkins876@gmail.com>
2020-11-11 09:05:33 -0500

    firebaseConfig setup

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -10,6 +10,9 @@ export const Sidebar = () =>
     <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
   </ul>
   <div className="sidebar__middle">
-    
+    <span><FaChevronDown/></span>
+    <h2>Projects</h2>
   </div>
+  <ul className="sidebar__projects">Projects will be here</ul>
+  Add Project Component Here!!
 </div>
