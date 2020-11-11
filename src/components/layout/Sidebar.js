8246311e61116e2fded5fa1c30caead31072d9c0
sedx876 <swatkins876@gmail.com>
2020-11-11 08:28:27 -0500

    added styling and fixed sass denpendency

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar, } from 'react-icons/fa'
 
 
-export const Sidebar = () => <p>Sidebar Bitches</p>
+export const Sidebar = () => 
+<div className="sidebar" data-testid="sidebar">
+  <ul className="sidebar__generic">
+    <li><span><FaInbox/></span><span>Inbox</span></li>
+    <li><span><FaRegCalendar/></span><span>Today</span></li>
+    <li><span><FaRegCalendarAlt/></span><span>Next 7 days</span></li>
+  </ul>
+  <div className="sidebar__middle">
+    
+  </div>
+</div>
