b18c935d3d0cf81b3759d1892c210f66f4e9da03
sedx876 <swatkins876@gmail.com>
2020-11-12 07:52:18 -0500

    added individualproject.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Header } from './components/layout/Header'
 import { Content } from './components/layout/Content'
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 
 export const App = () => {
+
+  
   return (
-    <div className="App">
-     <Header/>
-     <Content/>
-    </div>
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header/>
+          <Content/>
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
   );
 }
 
